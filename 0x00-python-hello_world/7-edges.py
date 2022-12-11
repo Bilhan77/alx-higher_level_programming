5a6aa0f055176c8e5fba846a532752d93f1c490a
Prince Stephen <Toughprince8968@gmail.com>
2022-12-11 12:20:24 -0800

    seventh

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+word[:3] = word_first_3
+word[8:9] = word_last_2
+word[4:7] = middle_word
+print(f"First 3 letters: {word_first_3}")
+print(f"Last two letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
